7399b4baaa7ba77afec3e5aceb21c098b8ca7b96
aarcThom <thomas@aarc.io>
2022-10-26 02:50:04 -0700

    Finished Fibbonaci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibArr = [1,1];
+
+    for (i = 2; i < num; i++){
+        fibArr.push(fibArr[i-1] + fibArr[i-2]);
+    }
+    return fibArr[num-1] ?? 'OOPS';
 
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
