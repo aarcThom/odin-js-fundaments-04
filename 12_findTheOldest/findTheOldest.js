7dbf25ef721093744c9489a23ca65254097151b3
aarcThom <thomas@aarc.io>
2022-10-26 02:44:20 -0700

    Finish 'find the oldest' using array.sort()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const orderedPeople = people.sort((a,b) => getAge(a) > getAge(b) ? 1 : -1);
+    return orderedPeople.slice(-1)[0];
 };
+
+function getAge(person) {
+    const endDate = person.yearOfDeath ?? new Date().getFullYear();
+    return endDate - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
