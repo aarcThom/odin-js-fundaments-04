2d8b856163870ab84709a14546adc1d4e14d6e6b
aarcThom <thomas@aarc.io>
2022-10-26 00:37:04 -0700

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (strIN) {
+    strIN = strIN.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    const revStr = strIN.split('').reverse().join('');
+    
+    return strIN === revStr;
 };
 
 // Do not edit below this line
